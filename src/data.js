a2c6722e4633f9c5e7728e071d3e8596961a07e4
LukaszPudlewski <98075284+LukaszPudlewski@users.noreply.github.com>
2022-06-18 11:36:25 +0100

    slider

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://images.unsplash.com/photo-1572804013427-4d7ca7268217?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80",
+                
+      title: "SALE",
+      desc: "THE BEST SALE OUT THERE",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://images.unsplash.com/photo-1585487000160-6ebcfceb0d03?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=434&q=80",
+      
+      title: "POPULAR",
+      desc: "VERY POPULAR, BUY IT",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://images.unsplash.com/photo-1589150339379-edfb6f86b7b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    
+      title: "TITLE",
+      desc: "SOME MORE CLOTHS",
+      bg: "fbf0f4",
+    },
+  ];
