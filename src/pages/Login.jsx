2492e085b09c5aeee49cc7312f37770e659bf8cf
LukaszPudlewski <98075284+LukaszPudlewski@users.noreply.github.com>
2022-06-20 21:17:35 +0100

    20.06

---
+++ src/pages/Login.jsx
@@ -0,0 +1,93 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://images.unsplash.com/photo-1541234465457-7930d040edeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 25%;
+  padding: 20px;
+  background-color: teal;
+  justify-content: center;
+  display: flex;
+  opacity: 0.95;
+`;
+
+const Title = styled.h1`
+  font-size: 40px;
+  font-weight: 500;
+  text-align: center;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  opacity: 0.8;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 90%;
+  margin: 10px 0;
+  padding: 10px;
+`;
+
+const Button = styled.button`
+  width: 50%;
+  margin-top: 30px;
+  margin-bottom: 30px;
+  border: none;
+  padding: 15px 20px;
+  background-color: gray;
+  color: white;
+  cursor: pointer;
+
+  &:hover {
+    color: gray;
+    background-color: white;
+  }
+`;
+
+const Link = styled.a`
+  margin: 10px 10px;
+  font-size: 16px;
+  font-weight: 200;
+  text-decoration: underline;
+  cursor: pointer;
+
+  &:hover {
+    color: white;
+  }
+`;
+
+const Login = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Form>
+          <Title>SIGN IN</Title>
+          <Input placeholder="username" />
+          <Input placeholder="password" />
+          <Button>LOGIN</Button>
+          <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
+          <Link>CREATE A NEW ACCOUNT</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Login
---
+++ src/pages/Register.jsx
@@ -0,0 +1,84 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://images.unsplash.com/photo-1496747611176-843222e1e57c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80")
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 40%;
+  padding: 20px;
+  background-color: teal;
+`;
+
+const Title = styled.h1`
+  font-size: 40px;
+  font-weight: 500;
+  text-align: center;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+  opacity: 0.8;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+`;
+
+const Button = styled.button`
+  width: 50%;
+  margin-top: 30px;;
+  border: none;
+  padding: 15px 20px;
+  background-color: gray;
+  color: white;
+  cursor: pointer;
+
+  &:hover {
+    color: gray;
+    background-color: white;
+  }
+`;
+const ButtonDiv = styled.div`
+  display:flex;
+  flex: 2;
+  justify-content: center;
+`
+
+const Login = () => {
+  return (
+    <Container>
+        <Wrapper>
+            <Title>CREATE AN ACCOUNT</Title>
+            <Form>
+                <Input placeholder="first name"/>
+                <Input placeholder="last name"/>
+                <Input placeholder="username"/>
+                <Input placeholder="email"/>
+                <Input type="password" placeholder="password"/>
+                <Input type="password" placeholder="confirm password"/>
+                <ButtonDiv>
+                <Button>CREATE ACCOUNT</Button>
+                </ButtonDiv>
+            </Form>
+        </Wrapper>
+    </Container>
+  )
+}
+
+export default Login
