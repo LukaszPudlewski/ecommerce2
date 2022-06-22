cbc5d2e6316513c2e86cf5536919ee43c662c33c
LukaszPudlewski <98075284+LukaszPudlewski@users.noreply.github.com>
2022-06-22 13:41:33 +0100

    final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import { Badge } from '@material-ui/core';
+import { mobile } from '../responsive';
 
 const Container = styled.div`
     height: 60px;
+    ${mobile({ height: "100px" })}
 `
 
 const Wrapper = styled.div`
@@ -12,6 +14,7 @@ padding: 10px 20px;
 display: flex;
 justify-content: space-between;
 align-items: center;
+${mobile({ padding: "10px 0px" })}
 `
 
 const Left = styled.div`
@@ -22,6 +25,7 @@ align-items: center;
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
+  ${mobile({ display: "none" })}
 `
 const SearchContainer = styled.div`
   border: 0.5px solid lightgray;
@@ -32,6 +36,7 @@ const SearchContainer = styled.div`
 `
 const Input = styled.input`
   border: none;
+  ${mobile({ width: "50px" })}
 `
 
 const Center = styled.div`
@@ -40,6 +45,7 @@ text-align: center;
 `
 const Logo = styled.h1`
   font-weight: bold;
+  ${mobile({ fontSize: "24px" })}
 `
 
 const Right = styled.div`
@@ -47,11 +53,13 @@ flex: 1;
 display: flex;
 align-items: center;
 justify-content: flex-end;
+${mobile({ flex: 2, justifyContent: "center" })}
 `
 const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
   margin-left: 30px;
+  ${mobile({ fontSize: "12px", marginLeft: "10px" })}
 `
 
 const Navbar = () => {
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { mobile } from '../responsive';
 
 const Container = styled.div`
   width: 100vw;
@@ -19,6 +20,7 @@ const Wrapper = styled.div`
   width: 40%;
   padding: 20px;
   background-color: teal;
+  ${mobile({ width: "75%" })}
 `;
 
 const Title = styled.h1`
